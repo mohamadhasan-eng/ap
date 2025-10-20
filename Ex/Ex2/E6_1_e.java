562faceb0c0abb01b2c7cb37cd1239f05a21403d
mohammadhasann <mohamadhasanf71@gmail.com>
2025-10-20 10:42:40 +0330

    E6_1_e

---
+++ Ex/Ex2/E6_1_e.java
@@ -0,0 +1,21 @@
+package Ex.Ex2;
+
+import java.util.Scanner;
+
+public class E6_1_e {
+    public static void main(String[] args) {
+        System.out.println("Please enter a number: ");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int r = 0;
+        int res = 0;
+        while (num != 0) {
+            r = num % 10;
+            num = num / 10;
+            if (r % 2 != 0) {
+                res += r;
+            }
+        }
+        System.out.println("the sum of the odd digits of this number is: " + res);
+    }
+}
