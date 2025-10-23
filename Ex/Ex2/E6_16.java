b06d3c5c5738d0f6361c5ae2459b04f18a6b8eb2
mohammadhasann <mohamadhasanf71@gmail.com>
2025-10-23 18:30:57 +0330

    E6_16

---
+++ Ex/Ex2/E6_16.java
@@ -0,0 +1,21 @@
+package Ex.Ex2;
+
+public class E6_16 {
+    public static void main(String[] args) {
+        System.out.println("Multiplication table:");
+        int[][] multiTable = new int[10][10];
+
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                multiTable[i][j] = (i + 1) * (j + 1);
+            }
+        }
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print(multiTable[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
