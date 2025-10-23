9b47ecd1885252696cc29b4964a1c18a37ad55a0
mohammadhasann <mohamadhasanf71@gmail.com>
2025-10-23 17:44:19 +0330

    E6_13

---
+++ Ex/Ex2/E6_13.java
@@ -0,0 +1,18 @@
+package Ex.Ex2;
+
+import java.util.Scanner;
+
+public class E6_13 {
+    public static void main(String[] args) {
+        System.out.println("Please enter a decimal number: ");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        System.out.println("Binary equivalent of number:");
+        while (num != 0){
+            int r = num % 2;
+            num /= 2;
+            System.out.println(r);
+        }
+    }
+}
