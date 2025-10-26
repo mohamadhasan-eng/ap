c95195edf7fb9f540be06398d8ed294cef4e5656
mohammadhasann <mohamadhasanf71@gmail.com>
2025-10-26 23:04:40 +0330

    E_6_13

--- Ex/Ex2/E6_13.java
+++ Ex/Ex2/E6_13.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class E6_13 {
     public static void main(String[] args) {
+        System.out.println();
         System.out.println("Please enter a decimal number: ");
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
