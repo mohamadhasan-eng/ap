97f19b6e3e8438d96ff02f3a60d487ca2281a051
mohammadhasann <mohamadhasanf71@gmail.com>
2025-10-26 23:00:44 +0330

    E_6_3

--- Ex/Ex2/E6_3.java
+++ Ex/Ex2/E6_3.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class E6_3 {
     public static void main(String[] args) {
+        System.out.println();
         System.out.println("Please enter a String: ");
         Scanner scanner = new Scanner(System.in);
         String string = scanner.nextLine();
