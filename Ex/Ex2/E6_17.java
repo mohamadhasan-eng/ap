c7cd0875bea1dffa3bc77ec125d1c4449d336852
mohammadhasann <mohamadhasanf71@gmail.com>
2025-10-26 23:11:27 +0330

    E_6_17

--- Ex/Ex2/E6_17.java
+++ Ex/Ex2/E6_17.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class E6_17 {
     public static void main(String[] args) {
-
+        System.out.println();
         System.out.println("Please entter a number(k): ");
         Scanner scanner = new Scanner(System.in);
         int k = scanner.nextInt();
